c0a632167db76c78fb7cbfa68aca5802d1a83982
Mironov Evgeny <e.d.mironov@yandex.ru>
2026-07-27 19:47:20 +0300

    initial commit

---
+++ src/main/java/ru/netology/cloud_service_diploma/CloudServiceDiplomaApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.cloud_service_diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServiceDiplomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServiceDiplomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/cloud_service_diploma/CloudServiceDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.cloud_service_diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceDiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
